b531bb65a9166d6915824353f9f0cbbb315fe4fb
LSatoshi <leandro.satoshi@usp.br>
2020-11-25 14:06:15 -0300

    added some visual updates

--- PodraoSimulator/Assets/Scripts/GameManager.cs
+++ PodraoSimulator/Assets/Scripts/GameManager.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
     public Player player;
+    public Ingredient[] ingredients;
+    public Recipe[] recipes;
+    public Text[] ingTexts;
+
 
 
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        int i = 0;
+        foreach(Ingredient ing in ingredients) {
+            ingTexts[i].text = ing.name + ": quantidade: " +ing.quantity; 
+            i++;
+        }
         
     }
 }
